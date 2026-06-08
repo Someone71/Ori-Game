2b0948ab98cbba52d7278471dff842d154885d8d
Bryan <bryanxiao0001@gmail.com>
2026-06-08 14:32:23 -0400

    e

--- Scripts/projectile_shooter.gd
+++ Scripts/projectile_shooter.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-
+@onready var main = get_tree().get_root().get_node("OriGame")
+@onready var projectile = load("res://Scenes/MapProjectile.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func shoot():
+	var instance = projectile.instantiate()
+	instance.dir = rotation
+	instance.spawnPos = global_position
+	main.add_child.call_deffered(instance)
