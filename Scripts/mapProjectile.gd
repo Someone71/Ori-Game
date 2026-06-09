836d57e33726642eb41c8d513ef56782d40b6dd2
SantiFdlm <sanfra27@bergen.org>
2026-06-09 13:54:31 -0400

    Update mapProjectile.gd

--- Scripts/mapProjectile.gd
+++ Scripts/mapProjectile.gd
@@ -1,14 +1,12 @@
 extends Area2D
 
 @export var speed : int = 200
-@export var mass = 1.0
+@export var mass = 1
 var velocity: Vector2 = Vector2.ZERO
 
 func setup(dir: Vector2) -> void:
-	velocity = dir * speed + 9.8*Vector2.DOWN
+	velocity = dir * speed + 10*Vector2.DOWN
 	rotation = dir.angle()
 
 func _physics_process(delta: float) -> void:
-	#pass
-	direction = Vector2.RIGHT.rotated(rotation)
-	global_position += direction * speed * delta
+	position += velocity * delta
