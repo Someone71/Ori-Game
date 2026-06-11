3b1920809b134ff0cd96552f63e72f255b5d270f
SantiFdlm <sanfra27@bergen.org>
2026-06-11 14:10:46 -0400

    e
    e

--- Scripts/player_projectile.gd
+++ Scripts/player_projectile.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
-@export var speed : int = 200
+@export var speed : int = 500
 @export var mass = 1.0
 var velocity: Vector2 = Vector2.ZERO
 
 func setup(dir: Vector2) -> void:
-	velocity = dir * speed + 10*Vector2.DOWN
+	velocity = dir * speed
 	rotation = dir.angle()
 
 func _physics_process(delta: float) -> void:
+	velocity.y += 0.5*gravity * mass * delta
 	position += velocity * delta
